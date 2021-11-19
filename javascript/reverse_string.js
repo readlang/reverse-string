590495faa25c6ebcccac5904ff4f19369bb7ef50
Read Langworthy <rlangworthy@gmail.com>
2021-11-18 20:52:22 -0800

    commit tree 4434a240728b22ccefa567b6fed44b3400f18dd2

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,12 @@
 function reverseString(str) {
   // type your code here
+  let newString = ""
+  for (let index = 0; index < str.length; index++) {
+    newString = newString + str[str.length-1-index]
+    
+  }
+
+  return newString
 }
 
 if (require.main === module) {
@@ -17,3 +24,6 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+console.log( reverseString( "hellocat" ) )
